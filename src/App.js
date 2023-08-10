ede5b427309d96e797204ce3c613761114d661e9
Pieter Ruslim <par3@sfu.ca>
2023-08-09 20:00:11 -0700

    Added Firebase Auth signup page

--- src/App.js
+++ src/App.js
@@ -1,22 +1,29 @@
-import './App.css';
-import SearchBar from './Components/SearchBar';
-import 'bootstrap/dist/css/bootstrap.min.css';
-import BookGrid from './Components/BookGrid';
-import { useEffect, useState } from 'react';
-import { BrowserRouter, Route, Routes, useSearchParams} from 'react-router-dom';
-import Search from './Components/Search';
-import BookInfo from './Components/BookInfo';
+import "./App.css";
+import SearchBar from "./Components/SearchBar";
+import "bootstrap/dist/css/bootstrap.min.css";
+import BookGrid from "./Components/BookGrid";
+import { useEffect, useState } from "react";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import Search from "./Components/Search";
+import BookInfo from "./Components/BookInfo";
+import { Button } from "react-bootstrap";
+import Login from "./Components/Login";
+import SignUp from "./Components/SignUp";
 
 function App() {
   return (
-    <BrowserRouter>
-      <Routes>
-        <Route path="/" element={<Search/>}/>
-        <Route path="/book" element={<BookInfo />}/>
-      </Routes>
-    </BrowserRouter>
-  )
+    <>
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<Search />} />
+          <Route path="/search" element={<Search />} />
+          <Route path="/book" element={<BookInfo />} />
+          <Route path="/login" element={<Login />} />
+          <Route path="/sign-up" element={<SignUp />} />
+        </Routes>
+      </BrowserRouter>
+    </>
+  );
 }
-
 
 export default App;
---
+++ src/Components/SignUp.js
@@ -0,0 +1,60 @@
+import { Button } from "react-bootstrap";
+import { useNavigate } from "react-router-dom";
+import { useState } from "react";
+import { auth } from "../firebase-config";
+import { createUserWithEmailAndPassword } from "firebase/auth";
+export default function SignUp() {
+  const navigate = useNavigate();
+
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+
+  const onSubmit = async (e) => {
+    e.preventDefault();
+
+    await createUserWithEmailAndPassword(auth, email, password)
+      .then((userCredential) => {
+        const user = userCredential.user;
+        console.log(user);
+        navigate("/login");
+      })
+      .catch((error) => {
+        const errorCode = error.code;
+        const errorMessage = error.message;
+        alert(errorMessage);
+        console.log(errorCode, errorMessage);
+      });
+  };
+  return (
+    <div className="container center-container">
+      <form>
+        <div class="form-group">
+          <label for="email">Email address</label>
+          <input
+            type="email"
+            class="form-control"
+            id="email"
+            value={email}
+            onChange={(e) => setEmail(e.target.value)}
+            aria-describedby="emailHelp"
+            placeholder="Enter email"
+          />
+        </div>
+        <div class="form-group">
+          <label for="password">Password</label>
+          <input
+            type="password"
+            class="form-control"
+            value={password}
+            onChange={(e) => setPassword(e.target.value)}
+            id="password"
+            placeholder="Password"
+          />
+        </div>
+        <button type="submit" class="btn btn-primary" onClick={onSubmit}>
+          Submit
+        </button>
+      </form>
+    </div>
+  );
+}
