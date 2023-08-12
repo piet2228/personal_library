1bbcbbb0929621fc6cbdd5cc12e3706617c45489
Pieter Ruslim <par3@sfu.ca>
2023-08-11 17:52:54 -0700

    Visual improvements to search

--- src/App.js
+++ src/App.js
@@ -9,16 +9,19 @@ import BookInfo from "./Components/BookInfo";
 import { Button } from "react-bootstrap";
 import Login from "./Components/Login";
 import SignUp from "./Components/SignUp";
+import NavBar from "./Components/NavBar";
+import AccountPage from "./Components/AccountPage";
 
 function App() {
   return (
     <>
+      <NavBar />
       <BrowserRouter>
         <Routes>
           <Route path="/" element={<Search />} />
           <Route path="/search" element={<Search />} />
           <Route path="/book" element={<BookInfo />} />
-          <Route path="/login" element={<Login />} />
+          <Route path="/login" element={<AccountPage />} />
           <Route path="/sign-up" element={<SignUp />} />
         </Routes>
       </BrowserRouter>
--- src/Components/SearchBar.js
+++ src/Components/SearchBar.js
@@ -1,25 +1,30 @@
-import { useState } from "react"
-import { Form } from "react-bootstrap";
-import { createSearchParams, useNavigate, useSearchParams} from "react-router-dom";
+import { useState } from "react";
+import { Button, Form, InputGroup } from "react-bootstrap";
+import {
+  createSearchParams,
+  useNavigate,
+  useSearchParams,
+} from "react-router-dom";
 
-export default function SearchBar(){
+export default function SearchBar() {
   const [queryParams] = useSearchParams();
-
+  const navigate = useNavigate();
   const [pageNum, setPageNum] = useState(queryParams.get("page"));
   const [searchStr, setSearchStr] = useState(queryParams.get("search_bar"));
 
   return (
-
-    <Form>
-      <input 
-        name="search_bar" 
-        value={searchStr}
-        type="search"
-        onChange={(event) => setSearchStr(event.target.value)}
-        />
-      <button>Search</button>
-
-
-    </Form>
+    <div className="m-3">
+      <Form>
+        <InputGroup>
+          <Form.Control
+            name="search_bar"
+            value={searchStr}
+            type="search"
+            onChange={(event) => setSearchStr(event.target.value)}
+          />
+          <Button href={`/search?search_bar=${searchStr}`}>Search</Button>
+        </InputGroup>
+      </Form>
+    </div>
   );
 }
