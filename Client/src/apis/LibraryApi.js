3d1c2feadde9501129229fccec2b2e05820bcf5b
Pieter Ruslim <par3@sfu.ca>
2023-08-19 15:20:34 -0700

    url changes depending on environment

--- Client/src/apis/LibraryApi.js
+++ Client/src/apis/LibraryApi.js
@@ -1,4 +1,6 @@
-const baseUrl = `http://${process.env.REACT_APP_SERVERHOST}:${process.env.REACT_APP_SERVERPORT}/api`;
-//TODO: change server listening to /api/books or /api/collection...
-//change urls in fetch requests to use this
+const baseUrl =
+  process.env.NODE_ENV === "production"
+    ? `/api`
+    : `http://${process.env.REACT_APP_SERVERHOST}:${process.env.REACT_APP_SERVERPORT}/api`;
+
 export default baseUrl;
